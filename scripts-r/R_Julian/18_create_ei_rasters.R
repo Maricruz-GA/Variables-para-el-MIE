24020efb428c547eae5be56d03b1dffc6a1a084c
Miguel Eq <equihuam@gmail.com>
2026-04-15 17:31:34 -0600

    script en r para crear mapa raster de ie

--- scripts-r/R_Julian/18_create_ei_rasters.R
+++ scripts-r/R_Julian/18_create_ei_rasters.R
@@ -1,7 +1,8 @@
-library("terra")
-library("data.table")
-library("dplyr")
-#library("raster")
+#library("terra")
+#library("data.table")
+#library("dplyr")
+#library("raster")   ------ version terra ya lo incluye
+pacman::p_load(dplyr, data.table, terra)
 
 dropbox_dir <- "C:/Users/equih/1 Nubes/Dropbox/ei-coastal"
 dat_csv <- paste0(dropbox_dir,"/data/cei_final_train_v1ask.csv")
