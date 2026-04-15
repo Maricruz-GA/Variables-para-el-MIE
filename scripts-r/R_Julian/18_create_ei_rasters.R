ada1aad4250709caac94ef32a5f8c245a5ea0210
Miguel Eq <equihuam@gmail.com>
2026-04-15 15:57:48 -0600

    script en r pars crear mapa raster de ie

--- scripts-r/R_Julian/18_create_ei_rasters.R
+++ scripts-r/R_Julian/18_create_ei_rasters.R
@@ -1,31 +1,46 @@
 library("terra")
 library("data.table")
+library("dplyr")
 #library("raster")
 
-dat <- fread("./data_training_tables/cei_final_train_v1ask.csv")
+dropbox_dir <- "C:/Users/equih/1 Nubes/Dropbox/ei-coastal"
+dat_csv <- paste0(dropbox_dir,"/data/cei_final_train_v1ask.csv")
+
+# Estos datos están en coordenadas geográficas
+dat <- fread(dat_csv)
 dat <- dat[,1:3]
 dat$idx <- 1:nrow(dat)
 
-regiones <- unique(dat$regionid.x)
+regiones <- unique(dat$regionId)
 head(dat)
 
-eipred <- fread("./ei_predictions/1_puerto/cei_final_ie_expected_port_5_equal.csv", data.table = FALSE)
+# Lee datos ie salida Netica
+datos_ie_file <- paste0(dropbox_dir, "/BN-results/EII-data/cei_final_ie_expected_port_5_equal_2026.csv")
+eipred <- fread(datos_ie_file, data.table = FALSE)
 min(as.numeric(unlist(eipred)))
 max(as.numeric(unlist(eipred)))
-eipred <- (as.numeric(unlist(eipred))-1.5)/(5.5-1.5)
-hist(eipred)
+
+# eipred <- (as.numeric(unlist(eipred))-1.5)/(5.5-1.5)
+eipred_norm <- eipred %>%
+  mutate(ie_norm = (`E[ei_qnint_map]` - 1.5) / (5.5 - 1.5)) %>%
+  select(ie_norm) %>%
+  unlist(use.names = FALSE)
+
+hist(eipred_norm)
 # Load corales shapefile.
-corales <- vect("./data_crude/08_coral-global/coral-global.shp")
+corales_shp <- paste0(dropbox_dir, "/data/data_crude/08_Corales/coral-global.shp")
+corales <- vect(corales_shp)
 
 # List coastal refetence grids.
-c_list <- list.files("./data/06_DunasCost250116_malla_ref_50m/",
+ref_grids_dir <- paste0(dropbox_dir, "/data/data_crude/DunasCost250116_malla_ref_50m/")
+c_list <- list.files(ref_grids_dir,
                      pattern = "\\.tif$",
                      full.names = TRUE,
                      recursive = TRUE)
 
 for (i in 1:length(regiones)){
 
-    region_ <- rast(c_list[i])
+    region_ <- rast(c_list[i])  # No se vuelve a usar
     
     region <- regiones[i]
     print(region)
@@ -45,6 +60,4 @@ for (i in 1:length(regiones)){
     output <- paste0("./ei_predictions/1_puerto/eicoastal_",region,".tif")
     writeRaster(ei_rast, output, overwrite=TRUE)
 }
-
-?writeRaster
 
